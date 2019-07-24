463662fe3809379126308d52fc145c089e2bc785
bkwillia052 <bkwillia052@gmail.com>
2019-07-24 14:36:49 -0500

    Updated sign icon design

--- src/components/SignImage.js
+++ src/components/SignImage.js
@@ -3,8 +3,10 @@ import "./SignImage.css";
 
 const SignImage = props => {
   return (
-    <div className="sign-icon">
+    <div className={`sign-icon ${props.sign}`}>
       <div className="sign-img-container">
+        <div className={`inner ${props.selected ? "square" : "circle"}`} />
+        <div className={`inner ${props.selected ? "circle" : "square"}`} />
         <img src={require(`../assets/zodiac-signs/${props.sign}.png`)} alt="" />
       </div>
       <div className="sign-name">{props.sign.toUpperCase()}</div>
